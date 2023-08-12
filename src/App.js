2455d39ad621e5946f3df3cf8c9281c803ecf743
shreensamhi <119866077+shreensamhi@users.noreply.github.com>
2023-08-12 19:11:44 +0300

    full project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
+import { useState } from 'react';
+import './index.css';
 
 function App() {
+  const [result, setResult] = useState("");
+  
+  const handClick = (e) =>{
+    setResult(result.concat(e.target.name));
+  }
+  const clear = () => {
+    setResult("");
+  }
+
+  const backspace = () => {
+    setResult(result.slice(0,result.length -1));
+
+  }
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch(err) {
+      setResult("Error")
+    }
+  }
+
+
   return (
-    <section>
-      <h1 className="title">Software Eng</h1>
-      <h1 className="title">Shreen</h1>
-  </  section>
+    <>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button className='highlight' onClick={clear} id='clear'>Clear</button>
+          <button className='highlight' onClick={backspace} id='backspace'>C</button>
+          <button className='highlight' name='/' onClick={handClick}>&divide;</button>
+          <button name='7' onClick={handClick}>7</button>
+          <button name='8' onClick={handClick}>8</button>
+          <button name='9' onClick={handClick}>9</button>
+          <button className='highlight' name='*' onClick={handClick}>&times;</button>
+          <button name='4' onClick={handClick}>4</button>
+          <button name='5' onClick={handClick}>5</button>
+          <button name='6' onClick={handClick}>6</button>
+          <button className='highlight' name='-' onClick={handClick}>&ndash;</button>
+          <button name='1' onClick={handClick}>1</button>
+          <button name='2' onClick={handClick}>2</button>
+          <button name='3' onClick={handClick}>3</button>
+          <button className='highlight' name='+' onClick={handClick}>+</button>
+          <button name='0' onClick={handClick}>0</button>
+          <button name='.' onClick={handClick}>.</button>
+          <button className='highlight' onClick={calculate} id='result'>=</button>
+        </div>
+      </div>
+    
+    </>
   );
 }
 
